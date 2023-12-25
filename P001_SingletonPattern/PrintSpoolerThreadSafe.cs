3b684af39636746190b7027b45dc92badaecfe83
26094245 <yunusemre.ulucay@arcelik.com>
2023-12-25 16:29:04 +0300

    singleton codes added

--- P001_SingletonPattern/PrintSpoolerThreadSafe.cs
+++ P001_SingletonPattern/PrintSpoolerThreadSafe.cs
@@ -2,5 +2,31 @@ namespace P001_SingletonPattern;
 
 public class PrintSpoolerThreadSafe
 {
-    
+    private static volatile PrintSpoolerThreadSafe instance;
+    private static readonly object syncRoot = new();
+
+    private PrintSpoolerThreadSafe()
+    {
+    }
+
+    public static PrintSpoolerThreadSafe GetInstance()
+    {
+        if (instance is null)
+        {
+            lock (syncRoot)
+            {
+                if (instance is null)
+                {
+                    instance = new PrintSpoolerThreadSafe();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public void Print(string document)
+    {
+        Console.WriteLine($"Printing: {document}");
+    }
 }
--- P001_SingletonPattern/Program.cs
+++ P001_SingletonPattern/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using P001_SingletonPattern;
 
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Task.Run(() => CallPrintSpoolerThreadSafe("Document 1"));
+        Task.Run(() => CallPrintSpoolerThreadSafe("Document 2"));
+
+        Thread.Sleep(200);
+
+        static void CallPrintSpoolerThreadSafe(string document)
+        {
+            PrintSpoolerThreadSafe spooler = PrintSpoolerThreadSafe.GetInstance();
+            spooler.Print(document);
+        }
+    }
+}
