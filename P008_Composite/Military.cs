c270caee7b3cd4bc14fed10e2629722e0f14b94f
26094245 <yunusemre.ulucay@arcelik.com>
2023-12-27 17:48:03 +0300

    conmposite done

---
+++ P008_Composite/Military.cs
@@ -0,0 +1,43 @@
+﻿namespace P008_Composite
+{
+    public interface IMilitaryUnit
+    {
+        void ConductOperation();
+        string ToString();
+    }
+
+    public class MilitaryUnit : IMilitaryUnit
+    {
+        private List<IMilitaryUnit> members = new();
+
+        public void AddMember(IMilitaryUnit member)
+        {
+            members.Add(member);
+        }
+
+        public void ConductOperation()
+        {
+            Console.WriteLine("Commanding the militar unit to conduct an operation");
+            
+            foreach (var member in members)
+            {
+                member.ConductOperation();
+            }
+        }
+    }
+
+    public class Soldier : IMilitaryUnit
+    {
+        private string name;
+
+        public Soldier(string name)
+        {
+            this.name = name;
+        }
+
+        public void ConductOperation() 
+        {
+            Console.WriteLine($"Soldier {name} is performing a specific task.");
+        }
+    }
+}
---
+++ P008_Composite/Program.cs
@@ -0,0 +1,17 @@
+﻿using P008_Composite;
+
+IMilitaryUnit soldier1 = new Soldier("John");
+IMilitaryUnit soldier2 = new Soldier("Alice");
+
+MilitaryUnit platoon = new MilitaryUnit();
+platoon.AddMember(soldier1);    
+platoon.AddMember(soldier2);
+
+MilitaryUnit otherPlatoon = new MilitaryUnit();
+otherPlatoon.AddMember(soldier2);
+
+MilitaryUnit company = new MilitaryUnit();
+company.AddMember(platoon);
+company.AddMember(otherPlatoon);
+
+company.ConductOperation();
